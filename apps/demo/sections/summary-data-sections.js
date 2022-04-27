f71a68f9541f44435fb573d34d122786f45777bf
Rhodine Orleans-Lindsay <Rhodine.Orleans-Lindsay@digital.homeoffice.gov.uk>
2022-04-27 10:57:48 +0100

    merge commit

--- apps/demo/sections/summary-data-sections.js
+++ apps/demo/sections/summary-data-sections.js
@@ -6,6 +6,7 @@ const APPEAL_STAGES = require('../lib/staticAppealStages').getstaticAppealStages
 const RRA_GROUPING = require('../lib/staticRraGrouping').getstaticRraGrouping();
 const RRA_LEVELS = require('../lib/staticRraLevels').getstaticRraLevels();
 const RRA_SCORES = require('../lib/staticRraScores').getstaticRraScores();
+const SFIA_SKILLS = require('../lib/staticSfiaSkills').getstaticSfiaSkills();
 const _ = require('lodash');
 
 module.exports = {
@@ -66,20 +67,16 @@ module.exports = {
       parse: v => _.get(_.find(RRA_LEVELS, group => group.value === v), 'label', '')
     }
   ],
-  'skill1': [
-    'sfiaSkill',
+  skill: [
+    {
+      field: 'rraSkill',
+      parse: v => _.get(_.find(SFIA_SKILLS, group => group.value === v), 'label', '')
+    },
     {
       field: 'rraScores',
       parse: v => _.get(_.find(RRA_SCORES, group => group.value === v), 'label', '')
     },
-    'rraEvidence'
-  ],
-  'skill2': [
-    'sfiaSkill2',
-    {
-      field: 'rraScores2',
-      parse: v => _.get(_.find(RRA_SCORES, group => group.value === v), 'label', '')
-    },
-    'rraEvidence2'
+    'rraEvidence',
+    'rraSupportingDocuments'
   ]
 };
