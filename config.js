a9d43ff032eef53e4ecae637b0a32245524095f7
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2022-10-04 18:26:48 +0100

    remove config defaults

--- config.js
+++ config.js
@@ -18,18 +18,17 @@ module.exports = {
       `http://localhost:${useMocks ? (process.env.PORT || 8080) : 3000}/file`
   },
   email: {
-    caseworker: process.env.CASEWORKER_EMAIL || 'sas-hof-test@digital.homeoffice.gov.uk',
-    notifyApiKey: process.env.NOTIFY_KEY ||
-    'hof_test-89548f6c-39cd-4acb-851c-1f4ffa2e479b-28426e56-443a-4ba4-98ed-fb576e717ed9',
-    notifyTemplate: process.env.NOTIFY_TEMPLATE || 'e3f83bba-3491-45ef-a9ba-984fbe0e27da',
-    notifyHigherTemplate: process.env.NOTIFY_HIGHER_TEMPLATE || '1fa1fe19-9d26-4e37-bbb5-358d9493f2a3',
-    templateUserAuthId: process.env.TEMPLATE_USER_AUTHORISATION_ID || 'b9176c11-6047-4f55-a190-5f08c32d6ad5',
-    from: process.env.FROM_ADDRESS || '',
-    replyTo: process.env.REPLY_TO || '',
-    accessKeyId: process.env.AWS_USER || '',
-    secretAccessKey: process.env.AWS_PASSWORD || '',
+    caseworker: process.env.CASEWORKER_EMAIL,
+    notifyApiKey: process.env.NOTIFY_KEY,
+    notifyTemplate: process.env.NOTIFY_TEMPLATE,
+    notifyHigherTemplate: process.env.NOTIFY_HIGHER_TEMPLATE,
+    templateUserAuthId: process.env.TEMPLATE_USER_AUTHORISATION_ID,
+    from: process.env.FROM_ADDRESS,
+    replyTo: process.env.REPLY_TO,
+    accessKeyId: process.env.AWS_USER,
+    secretAccessKey: process.env.AWS_PASSWORD,
     transportType: 'ses',
-    region: process.env.EMAIL_REGION || ''
+    region: process.env.EMAIL_REGION
   },
   hosts: {
     acceptanceTests: process.env.ACCEPTANCE_HOST_NAME || `http://localhost:${process.env.PORT || 8080}`
